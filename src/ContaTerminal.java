9c11ef6e8a6c9f6a1f0284f3f5cedbe5ab4d8c3d
OvoGit <marco.wktg@gmail.com>
2024-05-10 14:41:02 -0300

    termino do primeiro projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leitor = new Scanner(System.in);
+        
+        int numero;
+        double saldo = 237.48;
+        String agencia, nome_Cliente;
+        
+        System.out.println("Por favor, digite o seu nome");
+        nome_Cliente = leitor.next();
+        
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = leitor.next();
+        
+        System.out.println("Informe o número da sua conta");
+        numero = leitor.nextInt();
+        
+        System.out.println("Olá " + nome_Cliente + ", obrigado por criar uma conta em nosso banco, "
+                + "sua agência é " + agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
